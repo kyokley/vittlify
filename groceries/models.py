39a7e36780be9a6f8dfda6e52f365bd3db7b2898
Kevin Yokley <kyokley2@gmail.com>
2015-11-18 18:34:10 -0600

    Starting over

--- groceries/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class GroceryList(models.Model):
-    users = models.ManyToManyField('auth.user')
-
-class Item(models.Model):
-    name = models.CharField(max_length=200)
-    quantity = models.IntegerField(default=1)
