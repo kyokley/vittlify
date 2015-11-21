a5821901401853ffe745f15bbed633e93d121a67
Kevin Yokley <kyokley2@gmail.com>
2015-11-20 21:58:35 -0600

    Adding shopper

--- vittlify/groceries/models.py
+++ vittlify/groceries/models.py
@@ -1,8 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Item(models.Model):
     name = models.CharField(max_length=200)
     needed_quantity = models.IntegerField(default=1)
     got_quantity = models.IntegerField(default=0)
+    shopping_list = models.ForeignKey('ShoppingList')
     date_added = models.DateTimeField(auto_now_add=True)
     date_edited = models.DateTimeField(auto_now=True)
+
+class Shopper(models.Model):
+    user = models.OneToOneField(User)
+
+class ShoppingList(models.Model):
+    owner = models.ForeignKey('Shopper')
