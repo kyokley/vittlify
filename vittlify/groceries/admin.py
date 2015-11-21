e4f4ff2d132a66e8147350733694f559779d63ea
Kevin Yokley <kyokley2@gmail.com>
2015-11-21 16:46:47 -0600

    Setting up admin

--- vittlify/groceries/admin.py
+++ vittlify/groceries/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import (Item,
+                     Shopper,
+                     ShoppingList,
+                     )
 
 # Register your models here.
+admin.site.register(Item)
+admin.site.register(Shopper)
+admin.site.register(ShoppingList)
