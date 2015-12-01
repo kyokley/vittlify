34393c3882028643abb49eaa9b90037d38c40628
Kevin Yokley <kyokley2@gmail.com>
2015-11-30 20:56:12 -0600

    Adding post

--- vittlify/groceries/api_views.py
+++ vittlify/groceries/api_views.py
@@ -41,3 +41,10 @@ class ItemView(APIView):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def post(self, request, format=None):
+        serializer = ItemSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- vittlify/groceries/urls.py
+++ vittlify/groceries/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^signin/$', views.signin, name='signin'),
     url(r'^shopping_list_items/(?P<pk>[0-9]+)/$', api_views.ShoppingListItemsView.as_view(), name='shopping_list_items'),
     url(r'^item/(?P<pk>[0-9]+)/$', api_views.ItemView.as_view()),
+    url(r'^item/$', api_views.ItemView.as_view()),
 ]
