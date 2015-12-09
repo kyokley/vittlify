da2b6ec0b720341d9d527ab52e5e8f10793a88b6
Kevin Yokley <kyokley2@gmail.com>
2015-12-08 19:28:47 -0600

    Removing comments from template

--- vittlify/groceries/static/assets/groceries.js
+++ vittlify/groceries/static/assets/groceries.js
@@ -1,18 +1,20 @@
 var tables = {};
 function addItem(token, list_id){
-    var item_name = document.getElementById("new-item-name-" + list_id).value;
-    var item_comments = document.getElementById("new-item-comment-" + list_id).value;
+    var item_name = document.getElementById("new-item-name-" + list_id);
+    var item_comments = document.getElementById("new-item-comment-" + list_id);
     jQuery.ajax({url: "/vittlify/item/",
                  type: "POST",
                  dataType: "json",
                  data: {shopping_list_id: list_id,
-                        name: item_name,
-                        comments: item_comments,
+                        name: item_name.value,
+                        comments: item_comments.value,
                         csrfmiddlewaretoken: token},
                  success: function(json){
                      var table = tables["table-shopping_list-" + list_id];
                      var checkbox = '<input type="checkbox" id="checkbox-' + list_id + '-' + json.pk + '" name="checkbox-' + json.name + '" />';
-                     table.row.add([item_name, checkbox]).draw();
+                     table.row.add([item_name.value, checkbox]).draw();
+                     item_name.value = "";
+                     item_comments.value = "";
                  },
                  error: function(){
                      console.log("Failed");
