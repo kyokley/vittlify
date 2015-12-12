6b11164d20ef24ead824b6375b1f95adc4c746f0
Kevin Yokley <kyokley2@gmail.com>
2015-12-12 11:04:32 -0600

    Adding save functionality

--- vittlify/groceries/static/assets/groceries.js
+++ vittlify/groceries/static/assets/groceries.js
@@ -47,9 +47,13 @@ function addItem(list_id){
 
 function openItem(item_id, shopping_list_id){
     var shopping_list_div = document.getElementById("div-" + shopping_list_id);
+    var add_item_panel = document.getElementById("add-item-panel-" + shopping_list_id);
     var edit_item_panel = document.getElementById("edit-item-panel-" + shopping_list_id);
+    var edit_item_id_elem = document.getElementById("edit-item-id");
 
+    edit_item_id_elem.value = item_id;
     shopping_list_div.style.display = "none";
+    add_item_panel.style.display = "none";
     edit_item_panel.style.display = "block";
 
     jQuery.ajax({url: "/vittlify/item/" + item_id + "/",
@@ -71,8 +75,33 @@ function openItem(item_id, shopping_list_id){
 
 function closeEditPanel(shopping_list_id){
     var shopping_list_div = document.getElementById("div-" + shopping_list_id);
+    var add_item_panel = document.getElementById("add-item-panel-" + shopping_list_id);
     var edit_item_panel = document.getElementById("edit-item-panel-" + shopping_list_id);
+    var edit_item_id_elem = document.getElementById("edit-item-id");
 
+    edit_item_id_elem.value = "";
     shopping_list_div.style.display = "block";
+    add_item_panel.style.display = "block";
     edit_item_panel.style.display = "none";
+}
+
+function saveItem(shopping_list_id){
+     var edit_item_comment_elem = document.getElementById("edit-item-comment-" + shopping_list_id);
+    var edit_item_id_elem = document.getElementById("edit-item-id");
+
+    var item_id = edit_item_id_elem.value;
+
+    jQuery.ajax({url: "/vittlify/item/" + item_id + "/",
+                 type: "PUT",
+                 dataType: "json",
+                 data: {comments: edit_item_comment_elem.value,
+                        csrfmiddlewaretoken: token},
+                 success: function(json){
+                     closeEditPanel(shopping_list_id);
+                 },
+                 error: function(){
+                     alert("An error has occurred!");
+                     closeEditPanel(shopping_list_id);
+                 }
+    });
 }
