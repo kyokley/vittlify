13f97c1bc57e323fee5cd26042bea2c0858c7862
Kevin Yokley <kyokley2@gmail.com>
2015-12-18 07:51:01 -0600

    Commenting out WIP code

--- vittlify/groceries/static/assets/settings.js
+++ vittlify/groceries/static/assets/settings.js
@@ -1,16 +1,16 @@
 /*jslint browser:true */
 var token;
 
-function addShoppingList(){
-    var new_list_name = document.getElementById("new-list-name");
-    if(!new_list_name.value){
-        alert('Name is a required field for new shopping lists');
-    } else {
-        jQuery.ajax({url: "/vittlify/shopping_list/",
-                     type: "POST",
-                     dataType: "json",
-                     data: {owner_id: list_id,
-                            name: item_name.value,
-                            comments: item_comments.value,
-                            csrfmiddlewaretoken: token},
-                     success: function(json){
+//function addShoppingList(){
+//    var new_list_name = document.getElementById("new-list-name");
+//    if(!new_list_name.value){
+//        alert('Name is a required field for new shopping lists');
+//    } else {
+//        jQuery.ajax({url: "/vittlify/shopping_list/",
+//                     type: "POST",
+//                     dataType: "json",
+//                     data: {owner_id: list_id,
+//                            name: item_name.value,
+//                            comments: item_comments.value,
+//                            csrfmiddlewaretoken: token},
+//                     success: function(json){
