a9fb89998c986b44bb68753fdf096629970aad48
Kevin Yokley <kyokley2@gmail.com>
2016-01-02 14:01:34 -0600

    Adding NotifyAction objects to admin

--- vittlify/groceries/admin.py
+++ vittlify/groceries/admin.py
@@ -3,6 +3,7 @@ from .models import (Item,
                      Shopper,
                      ShoppingList,
                      ShoppingListMember,
+                     NotifyAction,
                      )
 
 # Register your models here.
@@ -10,3 +11,4 @@ admin.site.register(Item)
 admin.site.register(Shopper)
 admin.site.register(ShoppingList)
 admin.site.register(ShoppingListMember)
+admin.site.register(NotifyAction)
