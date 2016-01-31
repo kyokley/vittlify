ab30200a2c120ed868ead553336055536cc2d15b
Kevin Yokley <kyokley2@gmail.com>
2016-01-31 09:43:32 -0600

    Removing unnecessary dependencies

--- install_os_dependencies.sh
+++ install_os_dependencies.sh
@@ -4,4 +4,3 @@ sudo apt-get install postgresql -y
 sudo apt-get install postgresql-server-dev-9.3 -y
 sudo apt-get install libffi-dev -y
 sudo apt-get install postfix -y
-sudo apt-get install redis-server -y
