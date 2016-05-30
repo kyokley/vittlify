6f34e68a9c79b62c39b3ccf915d2aa8ea6b6196a
Kevin Yokley <kyokley2@gmail.com>
2016-05-30 14:11:24 -0500

    Adding tests

--- vittlify/groceries/tests.py
+++ vittlify/groceries/tests.py
@@ -1,7 +1,13 @@
 from django.test import TestCase
 
 from groceries.views import signin
+import mock
 
 class test_signin(TestCase):
     def setUp(self):
+        self.request = mock.MagicMock()
+        self.request.method = 'POST'
+
+    @mock.patch('groceries.views.SignInForm')
+    def test_case_insensitive_username(self, mock_SignInForm):
         pass
