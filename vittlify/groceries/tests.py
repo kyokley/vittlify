e3e0482160fd7f4c8fbf190a6893748963d2c2f2
Kevin Yokley <kyokley2@gmail.com>
2016-05-31 23:20:12 -0500

    Starting tests

--- vittlify/groceries/tests.py
+++ vittlify/groceries/tests.py
@@ -1,13 +1,47 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
 from groceries.views import signin
 import mock
 
-class test_signin(TestCase):
+class TestSignIn(TestCase):
     def setUp(self):
         self.request = mock.MagicMock()
         self.request.method = 'POST'
 
+        self.password = 'a_pass'
+        self.new_username = 'some_user'
+
+        self.form = mock.MagicMock()
+        self.form.cleaned_data = {'username': self.new_username,
+                                  'password': self.password}
+
+        self.new_user = User()
+        self.new_user.username = self.new_username
+        self.new_user.set_password(self.password)
+        self.new_user.save()
+
+    @mock.patch('groceries.views.login')
+    @mock.patch('groceries.views.authenticate')
     @mock.patch('groceries.views.SignInForm')
-    def test_case_insensitive_username(self, mock_SignInForm):
-        pass
+    def test_form_is_invalid(self,
+                             mock_SignInForm,
+                             mock_authenticate,
+                             mock_login):
+        self.form.is_valid.return_value = False
+        signin(self.request)
+
+        self.assertFalse(mock_authenticate.called)
+        self.assertFalse(mock_login.called)
+
+    @mock.patch('groceries.views.authenticate')
+    @mock.patch('groceries.views.SignInForm')
+    def test_case_insensitive_username(self,
+                                       mock_SignInForm,
+                                       mock_authenticate):
+        self.form.is_valid.return_value = True
+        mock_SignInForm.return_value = self.form
+
+        signin(self.request)
+        mock_authenticate.assert_called_once_with(username=self.new_username,
+                                                  password=self.password)
