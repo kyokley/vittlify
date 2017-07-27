c2c056ea2ea6afab4610708110aa58639ae38ac7
Kevin Yokley <kyokley2@gmail.com>
2017-07-26 22:37:03 -0500

    Using new method in ShoppingListCategorySerializer (BROKEN)

--- vittlify/groceries/serializers.py
+++ vittlify/groceries/serializers.py
@@ -52,11 +52,6 @@ class ItemSerializer(serializers.Serializer):
         instance.save()
         return instance
 
-class ShoppingListCategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ShoppingListCategory
-        fields = '__all__'
-
 class ShoppingListSerializer(serializers.Serializer):
     pk = serializers.IntegerField(read_only=True)
     owner_id = serializers.IntegerField()
@@ -80,6 +75,24 @@ class ShoppingListSerializer(serializers.Serializer):
         instance.name = validated_data.get('name', instance.name)
         instance.save()
         return instance
+
+class ShoppingListCategorySerializer(serializers.ModelSerializer):
+    pk = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(required=True)
+    shopping_list = ShoppingListSerializer(required=True)
+
+    #class Meta:
+        #model = ShoppingListCategory
+        #fields = '__all__'
+
+    def create(self, validated_data):
+        if not validated_data.get('name'):
+            raise ValueError('Name must be provided for a new ShoppingListCategory object')
+        if not validated_data.get('shopping_list_id'):
+            raise ValueError('ShoppingLlistId must be provided for a new ShoppingListCategory object')
+        new_category = ShoppingListCategory.new(**validated_data)
+        return new_category
+
 
 class ShopperSerializer(serializers.ModelSerializer):
     shopping_lists = ShoppingListSerializer(many=True, read_only=True)
